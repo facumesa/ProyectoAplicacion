c14782cc257079ace3401467cc349c06b80154c9
Facundo Mesa <facundomesababon@gmail.com>
2025-08-03 23:13:53 -0300

    Add files via upload

---
+++ Aplicación/src/js/Cliente.js
@@ -0,0 +1,8 @@
+class Cliente {
+    constructor(_nombreUsuario, _Contrasenia, _nombrePerro, _tamanioPerro) {
+        this.nombreUsuario = _nombreUsuario;
+        this.Contrasenia = _Contrasenia;
+        this.nombrePerro = _nombrePerro;
+        this.tamanioPerro= _tamanioPerro;
+    }
+}
