95e5400cf8c23aed7bd3e96cbddb2d65f3ff45ea
Facundo Mesa <facundomesababon@gmail.com>
2025-08-03 23:17:14 -0300

    Delete Aplicación directory

--- Aplicación/src/js/Cliente.js
+++
@@ -1,8 +0,0 @@
-class Cliente {
-    constructor(_nombreUsuario, _Contrasenia, _nombrePerro, _tamanioPerro) {
-        this.nombreUsuario = _nombreUsuario;
-        this.Contrasenia = _Contrasenia;
-        this.nombrePerro = _nombrePerro;
-        this.tamanioPerro= _tamanioPerro;
-    }
-}
